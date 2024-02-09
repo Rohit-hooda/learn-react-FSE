a15a58af7313b4d802cc5162c87d4cfdf4590514
Rohit Hooda <rohithooda1999@gmail.com>
2024-02-09 16:31:10 -0500

    Added the Class changes for 09 Feb

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,20 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+// import Profile from './components/profile'
+import Profile from './qcomps/profile_mistake'
+import MyProfile from './qcomps/firstcomp';
+import Bio from './qcomps/bios'
+import PackingList from './qcomps/props_item'
+import Todo from './qcomps/todos'
 
 export default function Home() {
   return (
     <div className={styles.main}>
         <Profile />
+        <MyProfile />
+        <Bio />
+        <Todo />
     </div>
   )
 }
