0826ed558c14e2da1eecd1c2b63fc4afc9311eee
Rohit Hooda <rohithooda1999@gmail.com>
2024-02-16 23:07:44 -0500

    Class Activity 16th Feb 2024

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,41 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+<<<<<<< Updated upstream
 import Profile from './components/profile'
+=======
+// import Profile from './components/profile'
+import Profile from './qcomps/profile_mistake'
+import MyProfile from './qcomps/firstcomp';
+import Bio from './qcomps/bios'
+import PackingList from './qcomps/props_item'
+import Todo from './qcomps/todos'
+// import Gallery from './components/whyState'
+// import Gallery from './components/state'
+import Gallery from './qcomps/state'
+import Form from './qcomps/stuckForm'
+import FeedbackForm from './qcomps/thankYouCrash'
+import RequestTracker from './qcomps/shoppingCart'
+>>>>>>> Stashed changes
 
 export default function Home() {
   return (
     <div className={styles.main}>
+<<<<<<< Updated upstream
         <Profile />
+=======
+    {/* <Profile /> */}
+      {/* <MyComp /> */}
+      {/* <TodoList /> */}
+      {/* <Gallery /> */}
+      {/* <PackingList /> */}
+      {/* <List /> */}
+      {/* <RecipeList /> */}
+      {/* <Gallery /> */}
+      {/* <Form /> */}
+      {/* <FeedbackForm /> */}
+      <RequestTracker />
+>>>>>>> Stashed changes
     </div>
   )
 }
